4c5c37cb5db35ca4ca41ac2d263a9630c0640cad
xiaoyaocz <xiaoyaocz@52uwp.com>
2023-06-16 14:04:28 +0800

    release 1.0.3

--- simple_live_app/lib/modules/other/debug_log_page.dart
+++ simple_live_app/lib/modules/other/debug_log_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:cross_file/cross_file.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';

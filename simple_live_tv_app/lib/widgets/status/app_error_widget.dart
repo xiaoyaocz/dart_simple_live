5a3f0a9178e626b053e5d479a1675885e6dc8740
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-03-13 17:19:24 +0800

    TV端搜索错误处理

--- simple_live_tv_app/lib/widgets/status/app_error_widget.dart
+++ simple_live_tv_app/lib/widgets/status/app_error_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:simple_live_tv_app/app/app_style.dart';
 import 'package:lottie/lottie.dart';
 
@@ -11,27 +12,23 @@ class AppErrorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: GestureDetector(
-        onTap: () {
-          onRefresh?.call();
-        },
-        child: Padding(
-          padding: AppStyle.edgeInsetsA12,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              LottieBuilder.asset(
-                'assets/lotties/error.json',
-                width: 260,
-                repeat: false,
-              ),
-              Text(
-                "$errorMsg\r\n点击刷新",
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 12, color: Colors.grey),
-              ),
-            ],
-          ),
+      child: Padding(
+        padding: AppStyle.edgeInsetsA12,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            LottieBuilder.asset(
+              'assets/lotties/error.json',
+              width: 200.w,
+              repeat: false,
+            ),
+            AppStyle.vGap24,
+            Text(
+              errorMsg.isEmpty ? "出错了，请稍后重试" : errorMsg,
+              textAlign: TextAlign.center,
+              style: AppStyle.textStyleWhite,
+            ),
+          ],
         ),
       ),
     );

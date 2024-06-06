f700273aad78027ff851d1f8b9fa1e688e23484c
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-06-06 08:30:44 +0800

    测试motion_sensors

--- simple_live_app/lib/modules/user/test_page.dart
+++ simple_live_app/lib/modules/user/test_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:simple_live_app/app/log.dart';
+import 'package:motion_sensors/motion_sensors.dart' as motion_sensors;
 
 class TestPage extends GetView<TestController> {
   const TestPage({super.key});
@@ -43,12 +44,38 @@ class TestController extends GetxController {
   }
 
   StreamSubscription<AccelerometerEvent>? _streamSubscription;
-
+  StreamSubscription? _orientationStreamSubscription;
   void listenSensor() {
     // 监听传感器事件，获取当前设备的方向
-    _streamSubscription = accelerometerEventStream(
-      samplingPeriod: const Duration(seconds: 1),
-    ).listen((AccelerometerEvent event) {
+    // _streamSubscription = accelerometerEventStream(
+    //   samplingPeriod: const Duration(seconds: 1),
+    // ).listen((AccelerometerEvent event) {
+    //   var x = event.x;
+    //   var y = event.y;
+
+    //   var newOrientation = "";
+    //   if (x >= 9.0 && y < 9.0 && y >= -9.0) {
+    //     newOrientation = "LANDSCAPE LEFT";
+    //   } else if (x <= -9.0 && y < 9.0 && y >= -9.0) {
+    //     newOrientation = "LANDSCAPE RIGHT";
+    //   }
+    //   if (orientation.value != newOrientation && newOrientation.isNotEmpty) {
+    //     orientation.value = newOrientation;
+    //     if (orientation.value == "LANDSCAPE RIGHT") {
+    //       SystemChrome.setPreferredOrientations([
+    //         DeviceOrientation.landscapeRight,
+    //       ]);
+    //     } else {
+    //       SystemChrome.setPreferredOrientations([
+    //         DeviceOrientation.landscapeLeft,
+    //       ]);
+    //     }
+    //     Log.d("newOrientation: $newOrientation");
+    //   }
+    // });
+
+    _orientationStreamSubscription =
+        motion_sensors.motionSensors.accelerometer.listen((event) {
       var x = event.x;
       var y = event.y;
 
@@ -78,6 +105,7 @@ class TestController extends GetxController {
   void onClose() {
     SystemChrome.setPreferredOrientations(DeviceOrientation.values);
     _streamSubscription?.cancel();
+    _orientationStreamSubscription?.cancel();
     super.onClose();
   }
 }

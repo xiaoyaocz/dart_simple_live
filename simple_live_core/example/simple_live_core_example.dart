c54569f6eb9f5d7a13246a83a95d17e878ef6d6a
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-06-07 14:18:30 +0800

    修复无法通过RoomID获取原画

--- simple_live_core/example/simple_live_core_example.dart
+++ simple_live_core/example/simple_live_core_example.dart
@@ -20,7 +20,7 @@ void main() async {
   };
   //var categores = await site.getCategores();
   //print(categores.length);
-  var detail = await site.getRoomDetail(roomId: "121118665759");
+  var detail = await site.getRoomDetail(roomId: "7377636198720015167");
   var playQualites = await site.getPlayQualites(detail: detail);
   var playUrls =
       await site.getPlayUrls(detail: detail, quality: playQualites.first);

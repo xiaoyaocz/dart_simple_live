8477d3ad0d3673ec43854a764d5720dede7358f4
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-07-18 15:28:54 +0800

    恢复抖音搜索

--- simple_live_app/lib/modules/search/search_controller.dart
+++ simple_live_app/lib/modules/search/search_controller.dart
@@ -3,9 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:simple_live_app/app/constant.dart';
 import 'package:simple_live_app/app/sites.dart';
-import 'package:simple_live_app/modules/search/douyin/douyin_search_controller.dart';
 import 'package:simple_live_app/modules/search/search_list_controller.dart';
 
 class AppSearchController extends GetxController
@@ -25,9 +23,9 @@ class AppSearchController extends GetxController
       }
 
       index = currentIndex;
-      if (Sites.supportSites[index].id == Constant.kDouyin) {
-        return;
-      }
+      // if (Sites.supportSites[index].id == Constant.kDouyin) {
+      //   return;
+      // }
 
       var controller =
           Get.find<SearchListController>(tag: Sites.supportSites[index].id);
@@ -47,14 +45,14 @@ class AppSearchController extends GetxController
   @override
   void onInit() {
     for (var site in Sites.supportSites) {
-      if (site.id == Constant.kDouyin) {
-        Get.put(DouyinSearchController(site));
-      } else {
-        Get.put(
-          SearchListController(site),
-          tag: site.id,
-        );
-      }
+      // if (site.id == Constant.kDouyin) {
+      //   Get.put(DouyinSearchController(site));
+      // } else {
+      Get.put(
+        SearchListController(site),
+        tag: site.id,
+      );
+      //}
     }
 
     super.onInit();
@@ -65,23 +63,23 @@ class AppSearchController extends GetxController
       return;
     }
     for (var site in Sites.supportSites) {
-      if (site.id == Constant.kDouyin) {
-        var controller = Get.find<DouyinSearchController>();
-        controller.keyword = searchController.text;
-        controller.searchMode.value = searchMode.value;
-        controller.reloadWebView();
-      } else {
-        var controller = Get.find<SearchListController>(tag: site.id);
-        controller.clear();
-        controller.keyword = searchController.text;
-        controller.searchMode.value = searchMode.value;
-      }
+      // if (site.id == Constant.kDouyin) {
+      //   var controller = Get.find<DouyinSearchController>();
+      //   controller.keyword = searchController.text;
+      //   controller.searchMode.value = searchMode.value;
+      //   controller.reloadWebView();
+      // } else {
+      var controller = Get.find<SearchListController>(tag: site.id);
+      controller.clear();
+      controller.keyword = searchController.text;
+      controller.searchMode.value = searchMode.value;
+      //}
     }
-    if (Sites.supportSites[index].id != Constant.kDouyin) {
-      var controller =
-          Get.find<SearchListController>(tag: Sites.supportSites[index].id);
-      controller.refreshData();
-    }
+    // if (Sites.supportSites[index].id != Constant.kDouyin) {
+    var controller =
+        Get.find<SearchListController>(tag: Sites.supportSites[index].id);
+    controller.refreshData();
+    //}
   }
 
   @override

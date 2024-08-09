f3e8bd9da1f9d298490e8942d9fe8dad910ea09a
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-08-09 23:48:52 +0800

    完善远程同步

--- simple_live_app/lib/modules/remote_sync/remote_sync_page.dart
+++ simple_live_app/lib/modules/remote_sync/remote_sync_page.dart
@@ -15,6 +15,24 @@ class RemoteSyncPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("数据同步"),
+        actions: [
+          Visibility(
+            visible: GetPlatform.isAndroid || GetPlatform.isIOS,
+            child: TextButton.icon(
+              onPressed: () async {
+                var result = await Get.toNamed(RoutePath.kSyncScan);
+                if (result == null || result.isEmpty) {
+                  return;
+                }
+                if (result.length == 5) {
+                  Get.toNamed(RoutePath.kRemoteSyncRoom, arguments: result);
+                } else {}
+              },
+              icon: const Icon(Remix.qr_scan_line),
+              label: const Text("扫一扫"),
+            ),
+          ),
+        ],
       ),
       body: ListView(
         padding: AppStyle.edgeInsetsA12,

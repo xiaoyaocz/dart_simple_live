5bddb2e426ae549172adbb845fa8840e018b4086
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-08-13 11:51:13 +0800

    支持导入导出应用设置

--- simple_live_app/lib/modules/user/other/other_settings_controller.dart
+++ simple_live_app/lib/modules/user/other/other_settings_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
@@ -9,6 +10,8 @@ import 'package:simple_live_app/app/controller/app_settings_controller.dart';
 import 'package:simple_live_app/app/controller/base_controller.dart';
 import 'package:simple_live_app/app/log.dart';
 import 'package:path/path.dart' as p;
+import 'package:simple_live_app/app/utils.dart';
+import 'package:simple_live_app/services/local_storage_service.dart';
 
 class OtherSettingsController extends BaseController {
   RxList<LogFileModel> logFiles = <LogFileModel>[].obs;
@@ -123,6 +126,77 @@ class OtherSettingsController extends BaseController {
       await file.copy(filePath);
       SmartDialog.showToast("保存成功");
     }
+  }
+
+  void exportConfig() async {
+    try {
+      var config = LocalStorageService.instance.settingsBox.toMap();
+      var shield = LocalStorageService.instance.shieldBox.toMap();
+      var data = {
+        "type": "simple_live",
+        "platform": Platform.operatingSystem,
+        "version": 1,
+        "time": DateTime.now().millisecondsSinceEpoch,
+        "config": config,
+        "shield": shield
+      };
+
+      var filePath = await FilePicker.platform.saveFile(
+        allowedExtensions: ['json'],
+        type: FileType.custom,
+        fileName: "simple_live_config.json",
+      );
+      if (filePath != null) {
+        var file = File(filePath);
+        await file.writeAsString(jsonEncode(data));
+        SmartDialog.showToast("保存成功");
+      }
+    } catch (e) {
+      Log.logPrint(e);
+      SmartDialog.showToast("导入失败:$e");
+    }
+  }
+
+  void importConfig() async {
+    try {
+      var file = await FilePicker.platform.pickFiles(
+        allowedExtensions: ['json'],
+        type: FileType.custom,
+      );
+      if (file == null) {
+        return;
+      }
+      var filePath = file.files.single.path!;
+      var data = jsonDecode(await File(filePath).readAsString());
+      if (data["type"] != "simple_live") {
+        SmartDialog.showToast("不支持的配置文件");
+        return;
+      }
+      // 检查platform
+      if (data["platform"] != Platform.operatingSystem &&
+          !await Utils.showAlertDialog("导入配置文件平台不匹配,是否继续导入?", title: "平台不匹配")) {
+        return;
+      }
+      LocalStorageService.instance.settingsBox.clear();
+      LocalStorageService.instance.shieldBox.clear();
+      LocalStorageService.instance.settingsBox.putAll(data["config"]);
+      LocalStorageService.instance.shieldBox
+          .putAll(data["shield"].cast<String, String>());
+      SmartDialog.showToast("导入成功,重启生效");
+    } catch (e) {
+      Log.logPrint(e);
+      SmartDialog.showToast("导入失败:$e");
+    }
+  }
+
+  void resetDefaultConfig() {
+    Utils.showAlertDialog("是否重置所有配置为默认值?").then((value) {
+      if (value) {
+        LocalStorageService.instance.settingsBox.clear();
+        LocalStorageService.instance.shieldBox.clear();
+        SmartDialog.showToast("重置成功,重启生效");
+      }
+    });
   }
 }
 

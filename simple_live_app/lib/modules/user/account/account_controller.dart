cda4008d77318b45f8e849a34d2d233b623ae019
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-08-13 14:34:10 +0800

    支持使用cookie登录哔哩哔哩 #463

--- simple_live_app/lib/modules/user/account/account_controller.dart
+++ simple_live_app/lib/modules/user/account/account_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simple_live_app/app/utils.dart';
-import 'package:simple_live_app/routes/app_navigation.dart';
+import 'package:simple_live_app/routes/route_path.dart';
 import 'package:simple_live_app/services/bilibili_account_service.dart';
 
 class AccountController extends GetxController {
@@ -11,7 +14,65 @@ class AccountController extends GetxController {
         BiliBiliAccountService.instance.logout();
       }
     } else {
-      AppNavigator.toBiliBiliLogin();
+      //AppNavigator.toBiliBiliLogin();
+      bilibiliLogin();
     }
+  }
+
+  void bilibiliLogin() {
+    Utils.showBottomSheet(
+      title: "登录哔哩哔哩",
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Visibility(
+            visible: Platform.isAndroid || Platform.isIOS,
+            child: ListTile(
+              leading: const Icon(Icons.account_circle_outlined),
+              title: const Text("Web登录"),
+              subtitle: const Text("填写用户名密码登录"),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {
+                Get.back();
+                Get.toNamed(RoutePath.kBiliBiliWebLogin);
+              },
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.qr_code),
+            title: const Text("扫码登录"),
+            subtitle: const Text("使用哔哩哔哩APP扫描二维码登录"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Get.back();
+              Get.toNamed(RoutePath.kBiliBiliQRLogin);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.edit_outlined),
+            title: const Text("Cookie登录"),
+            subtitle: const Text("手动输入Cookie登录"),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Get.back();
+              doCookieLogin();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void doCookieLogin() async {
+    var cookie = await Utils.showEditTextDialog(
+      "",
+      title: "请输入Cookie",
+      hintText: "请输入Cookie",
+    );
+    if (cookie == null || cookie.isEmpty) {
+      return;
+    }
+    BiliBiliAccountService.instance.setCookie(cookie);
+    await BiliBiliAccountService.instance.loadUserInfo();
   }
 }

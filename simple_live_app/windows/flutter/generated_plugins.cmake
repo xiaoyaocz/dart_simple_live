f3c766a5dc1f79b5350c9c9ea176fbd5c2806dd4
xiaoyaocz <xiaoyaocz@52uwp.com>
2025-04-11 10:57:49 +0800

    更新部分包版本

--- simple_live_app/windows/flutter/generated_plugins.cmake
+++ simple_live_app/windows/flutter/generated_plugins.cmake
@@ -10,7 +10,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_video
   permission_handler_windows
   screen_brightness_windows
-  screen_retriever
+  screen_retriever_windows
   share_plus
   url_launcher_windows
   volume_controller

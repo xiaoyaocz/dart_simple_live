f29fb9e04cb10ca514dfa8e1fc7e59c5cd50f873
xiaoyaocz <xiaoyaocz@52uwp.com>
2025-04-23 22:13:28 +0800

    回滚Flutter 3.22

--- simple_live_tv_app/windows/flutter/generated_plugins.cmake
+++ simple_live_tv_app/windows/flutter/generated_plugins.cmake
@@ -5,12 +5,11 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   media_kit_libs_windows_video
   media_kit_video
-  screen_brightness_windows
   url_launcher_windows
+  volume_controller
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  media_kit_native_event_loop
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)

960977f24e842c8d4003fe158eddda3303ade9ad
xiaoyaocz <xiaoyaocz@52uwp.com>
2025-04-24 11:46:57 +0800

    回滚部分包版本

--- simple_live_app/windows/flutter/generated_plugins.cmake
+++ simple_live_app/windows/flutter/generated_plugins.cmake
@@ -12,11 +12,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   screen_retriever_windows
   share_plus
   url_launcher_windows
-  volume_controller
   window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  media_kit_native_event_loop
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)

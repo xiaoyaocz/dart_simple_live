608356108d5dae9d3d33bdf4b89ed2bdb275fa27
xiaoyaocz <xiaoyaocz@52uwp.com>
2025-09-10 17:22:27 +0800

    修复抖音详情加载失败

--- simple_live_core/lib/src/douyin_site.dart
+++ simple_live_core/lib/src/douyin_site.dart
@@ -429,7 +429,7 @@ class DouyinSite implements LiveSite {
       },
     );
 
-    var renderData = RegExp(r'\{\\"state\\":\{\\"isLiveModal.*?\]\\n')
+    var renderData = RegExp(r'\{\\"state\\":\{\\"appStore.*?\]\\n')
             .firstMatch(result)
             ?.group(0) ??
         "";

e80e3ca04d64f2b6e8f236a721aa00a2ba5ee6cd
SlotSun <slot_sun@outlook.com>
2025-11-21 02:08:24 +0900

    chore: update some plugins

--- simple_live_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ simple_live_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,7 +14,6 @@ import media_kit_libs_macos_video
 import media_kit_video
 import network_info_plus
 import package_info_plus
-import path_provider_foundation
 import screen_brightness_macos
 import screen_retriever_macos
 import share_plus
@@ -33,7 +32,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
